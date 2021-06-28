125613f651a082f50d4f2657309137bc0c1d968f
Iijima_Tsubasa <tsubasa.iijima@sacn.co.jp>
2021-06-28 15:24:23 +0900

    06/28 15:24

--- portfolio/Threader/forms.py
+++ portfolio/Threader/forms.py
@@ -2,24 +2,24 @@ from django import forms
 from django.core.mail import EmailMessage
 
 class InquiryForm(forms.Form):
-    name=forms.CharField(label='お名前',max_length=30)
-    email=forms.EmailField(label='メールアドレス')
-    title=forms.CharField(label='タイトル',max_length=30)
-    message=forms.CharField(label='メッセージ',widget=forms.Textarea)
+    name=forms.CharField(label='name',max_length=30)
+    email=forms.EmailField(label='email')
+    title=forms.CharField(label='title',max_length=30)
+    message=forms.CharField(label='message',widget=forms.Textarea)
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
 
-        self.fields['name'].widget.attrs['class']='form-control col-9'
+        self.fields['name'].widget.attrs['class']='form-control col-7'
         self.fields['name'].widget.attrs['placeholder'] = 'お名前を記入してください。'
 
-        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['email'].widget.attrs['class'] = 'form-control col-7'
         self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスを記入してください。'
 
-        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['title'].widget.attrs['class'] = 'form-control col-7'
         self.fields['title'].widget.attrs['placeholder'] = 'タイトルを記入してください。'
 
-        self.fields['message'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['message'].widget.attrs['class'] = 'form-control col-7'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージを記入してください。'
 
     def send_email(self):
